0a956669121d093f4d98d8c3bab837e3fb83adc4
chibabe97 <chidinmaojukwu321@gmail.com>
2024-10-06 16:09:09 +0100

    fist commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Rice</li>
+      <li>Noodles</li>
+      <li>A whole bunch of proteins like beef,fish,turkey and chicken</li>
+    </ul>
+    <p>I am actually in love with good food, i want to have enough of them</p>
+  </div>,
+  document.getElementById("root")
+);
+
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
